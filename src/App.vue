e9a66110b1bb9112c41f4afa2e95adb490dbaf1c
Saurav Singh <saurav.singh@zensar.com>
2021-06-21 22:19:04 +0530

    real time chart

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,92 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+   <div class="chart-container">
+    <IEcharts :option="option" resizable :styles="{width:'100%', height:'400px'}"/>
+   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld';
+import IEcharts from "vue-echarts-v3/src/full.js";
+import io from "socket.io-client";
+var socket = io.connect("http://localhost:4000");
+
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  components:{
+    IEcharts
   },
+  data() {
+    return {
+        option : {
+            title: {
+                text: 'Line & Bar',
+                subtext: 'Real Time Chart'
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross',
+                    label: {
+                        backgroundColor: '#283b56'
+                    }
+                }
+            },
+            legend: {
+            },
+            xAxis: {
+                type: 'category',
+                data: []
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                name: 'BarChart',
+                data: [],
+                type: 'bar'
+            },
+            {
+                name: 'LineChart',
+                data: [],
+                type: 'line'
+            }]
+        }
+    }
+  },
+  methods:{
+    openSocketConnection(){
+       socket.on('line1', (fetchedData, fetchedTime ) => {
+          this.fillData(fetchedData,fetchedTime);
+       })
+       socket.on('line2', data => {
+          this.lineData(data);
+       })
+    },
+    fillData(data, time){
+        if (this.option.series[0].data.length > 10){
+           this.option.series[0].data.shift();
+           this.option.xAxis.data.shift();
+        }
+        let newTime = new Date(time).toTimeString();
+        let currentTime = newTime.split(" ")[0];
+        this.option.series[0].data.push(data);
+        this.option.xAxis.data.push(currentTime);
+    },
+    lineData(data){
+        if (this.option.series[0].data.length > 10){
+           this.option.series[1].data.shift();
+        }
+        this.option.series[1].data.push(data);
+    }
+  },
+  mounted(){
+    this.openSocketConnection();
+  }
 
-  data: () => ({
-    //
-  }),
-};
+}
 </script>
+<style scoped>
+  .chart-container{
+    box-shadow: 10px 5px 10px #cecece;
+    padding: 20px;
+    margin: 60px 60px;
+  }
+</style>
